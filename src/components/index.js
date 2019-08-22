3e2bff89aadef2ee256553f255551655acc77d1c
Alex James <alexjames@Alexs-MacBook-Pro.local>
2019-08-22 15:52:59 -0500

    added files to make crown component

--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,12 @@ export { default as Header } from './Header';
 export { default as Header__nav } from './Header__nav';
 export { default as Logo } from './Logo';
 
+export { default as Crest } from './Crest';
+export { default as CrestAnimator } from './CrestAnimator';
+export { default as CrestPoint } from './CrestPoint';
+export { default as CrestUser } from './CrestUser';
+export { default as Crown } from './Crown';
+
 export { default as Modal } from './Modal';
 export { default as DownloadModal } from './DownloadModal';
 export { default as AudentioModal } from './AudentioModal';
